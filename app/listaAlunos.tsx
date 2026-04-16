396aecd07ec5e9cc65f7b4879b7e74579ee1e593
DanKendi <dan.kendisa@gmail.com>
2026-04-15 22:11:21 -0300

    adicao botão del

--- app/listaAlunos.tsx
+++ app/listaAlunos.tsx
@@ -1,12 +1,14 @@
 import { firestore } from '@/services/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Text, TouchableOpacity, View } from 'react-native';
 
 interface Aluno {
     id: string;
     nome: string;
 }
+
+
 
 export default function Home(){
     const [alunos, setAlunos] = useState<Aluno[]>([]);
@@ -33,19 +35,48 @@ export default function Home(){
 
   if (loading) return <ActivityIndicator size="large" />;
 
+  const excluirAluno = async (id: string) => {
+  try {
+    // Cria a referência ao documento específico dentro da coleção
+    await deleteDoc(doc(firestore, 'Alunos', id));
+
+    // Remove do estado local para sumir da tela instantaneamente
+    setAlunos(alunos.filter(aluno => aluno.id !== id));
+    
+    alert("Aluno removido!");
+  } catch (error) {
+    console.error("Erro ao excluir:", error);
+  }
+};
+
 
     return (
             <View style={{ flex: 1 }}>
             <FlatList
-                data={alunos}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                <View style={{ padding: 10, borderBottomWidth: 1, borderColor: '#000' }}>
-                    <Text style={{ fontSize: 16 }}>{item.nome}</Text>
-                </View>
-                    )}
-                    ListEmptyComponent={<Text>Nenhum aluno encontrado.</Text>}
-      />
+      data={alunos}
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={{ marginTop: 20 }}
+      renderItem={({ item }) => (
+        <View style={{ 
+          flexDirection: 'row', 
+          justifyContent: 'space-between', 
+          alignItems: 'center',
+          padding: 15, 
+          borderBottomWidth: 1, 
+          borderColor: '#eee' 
+        }}>
+          <Text style={{ fontSize: 16 }}>{item.nome}</Text>
+          
+          {/* Botão de Excluir ao lado de cada nome */}
+          <TouchableOpacity 
+            onPress={() => excluirAluno(item.id)}
+            style={{ backgroundColor: '#e74c3c', padding: 8, borderRadius: 5 }}
+          >
+            <Text style={{ color: 'white' }}>Excluir</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    />
         </View>
     );
 
